86e88cf18bb99fed99bc8b181309c8609369373e
Siyu-Zhou <934050497@qq.com>
2017-05-16 09:40:55 -0400

    Update script.js

--- shopify-front-end-dev/script.js
+++ shopify-front-end-dev/script.js
@@ -13,11 +13,11 @@ function submitemailadd(){
 function checkInputContent(input){
     if(input){
     	console.log("Here");
-    	//if not click without empty, green text will show
+    	//if click without empty, green text will show
     	$("#clickwithempty").css("display","none");
         $("#senddata").css("display","inline");
     }else{
-        //if not click with empty, red text will show
+        //if click with empty, red text will show
     	console.log("empty");
         $("#senddata").css("display","none");
     	$("#clickwithempty").css("display","inline");
